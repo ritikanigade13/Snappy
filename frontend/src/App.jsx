7310cdd4a48f1cd53f9f573c9d538878fe7628d4
ritikanigade13 <89742522+ritikanigade13@users.noreply.github.com>
2024-05-29 23:26:14 +0530

    Home page UI added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,14 @@
 import "./App.css";
+import Home from "./pages/home/Home";
 import Login from "./pages/login/Login";
 import SignUp from "./pages/signup/SignUp";
 
 function App() {
   return (
     <>
-      <div className="p-4 h-screen flex items-center justify-center"></div>
-      <SignUp />
+      <div className="p-4 flex items-center justify-center"></div>
+      <Home />
+      
     </>
   );
 }
--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -1,11 +1,14 @@
-import React from 'react'
+import React from "react";
+import Sidebar from "../../components/sidebar/Sidebar";
+import MessageContainer from "../../components/messages/MessageContainer";
 
 function Home() {
   return (
-    <div>
-      
+    <div className="flex sm:h-[650px] md:h-[680px] rounded-sm overflow-hidden bg-gray-400 bg-clip-padding justify-between mx-20 mb-20 backdrop-filter backdrop-blur-md bg-opacity-5 ">
+      <Sidebar />
+      <MessageContainer />
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
